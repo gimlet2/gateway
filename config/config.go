7e365547bf4918189e29e2a356ebaaf3336502c5
gimlet2 <andrey.chernishov@gmail.com>
2018-02-17 17:20:40 +0100

    add routing logic

--- config/config.go
+++ config/config.go
@@ -3,6 +3,10 @@ package config
 import (
 	"encoding/json"
 	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/gimlet2/gateway/utils"
 )
 
 type API struct {
@@ -11,10 +15,18 @@ type API struct {
 }
 
 type Endpoint struct {
-	Name   string  `json:"name"`
-	Path   string  `json:"path"`
-	Sticky bool    `json:"sticky"`
-	Routes []Route `json:"routes"`
+	Name    string   `json:"name"`
+	Path    string   `json:"path"`
+	Sticky  bool     `json:"sticky"`
+	Routes  []Route  `json:"routes"`
+	Methods []string `json:"methods"`
+}
+
+func (e Endpoint) GetPath() string {
+	if e.Path == "" {
+		return e.Name
+	}
+	return e.Path
 }
 
 type Route struct {
@@ -23,14 +35,58 @@ type Route struct {
 	Match    Match    `json:"match"`
 }
 
+func (route Route) Matching(r *http.Request) bool {
+	return route.Match.Matching(r)
+}
+
 type Upstream struct {
 	Uri string `json:"uri"`
+}
+
+func (u Upstream) Forward(w http.ResponseWriter, r *http.Request) {
+	client := http.DefaultClient
+	var newRequest, _ = http.NewRequest(r.Method, u.Uri, r.Body)
+	resp, e := client.Do(newRequest)
+	if e != nil {
+		log.Printf("Error %e", e)
+	}
+	w.WriteHeader(resp.StatusCode)
+
+	for n, h := range resp.Header {
+		for _, s := range h {
+			w.Header().Add(n, s)
+		}
+	}
+	buff, _ := ioutil.ReadAll(resp.Body)
+	w.Write(buff)
+
 }
 
 type Match struct {
 	Headers map[string][]string `json:"headers,omitempty"`
 	Query   map[string][]string `json:"query,omitempty"`
 	Json    map[string][]string `json:"json,omitempty"`
+}
+
+func (m Match) Matching(r *http.Request) bool {
+	for n, v := range m.Headers {
+		if !utils.Contains(v, r.Header.Get(n)) {
+			return false
+		}
+	}
+	for n, v := range m.Query {
+		log.Printf("key %s, v %s", n, v)
+		if !utils.ContainsOne(v, map[string][]string(r.URL.Query())[n]) {
+			return false
+		}
+	}
+	// check json here
+	// for n, v := range m.Headers {
+	// 	if !utils.Contains(v, r.Header.Get(n)) {
+	// 		return false
+	// 	}
+	// }
+	return true
 }
 
 func Load(configFile string) API {
--- main.go
+++ main.go
@@ -1,35 +1,67 @@
 package main
 
 import (
-	"net/http"
-	"log"
 	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
 	"os"
+
 	"github.com/gimlet2/gateway/config"
+	"github.com/gimlet2/gateway/utils"
 )
 
 func main() {
 
 	log.Print("Start Server")
 	conf := config.Load(os.Getenv("config"))
-	http.HandleFunc("/config", func(w http.ResponseWriter, r *http.Request) {
+	get("/config", func(w http.ResponseWriter, r *http.Request) {
 		j, _ := json.Marshal(conf)
 		w.Write(j)
 	})
-	http.HandleFunc("/decision", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "POST" {
-			//decoder := json.NewDecoder(r.Body)
-			//var request DecisionRequest
-			//decoder.Decode(&request)
-			//log.Print(string(request.Data.Type))
-			//j, _ := json.Marshal(request)
-			//w.Write(j)
-		} else {
-			w.WriteHeader(http.StatusMethodNotAllowed)
-			w.Write([]byte("Error"))
-		}
-	})
+	for _, endpoint := range conf.Endpoints {
+		http.HandleFunc(endpoint.GetPath(), func(w http.ResponseWriter, r *http.Request) {
+			if len(endpoint.Methods) != 0 && !utils.Contains(endpoint.Methods, r.Method) {
+				writeErrorWithCode(w, http.StatusMethodNotAllowed, "Method is not allowed")
+				return
+			}
+			for _, route := range endpoint.Routes {
+				if route.Matching(r) {
+					route.Upstream.Forward(w, r);
+					break;
+				}
+			}
+
+		})
+	}
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
+func writeError(w http.ResponseWriter, message string) {
+	w.Write([]byte(fmt.Sprintf("{\"error\":\"%s\"}", message)))
+}
+
+func writeErrorWithCode(w http.ResponseWriter, code int, message string) {
+	w.WriteHeader(code)
+	w.Write([]byte(fmt.Sprintf("{\"error\":\"%s\"}", message)))
+}
+
+func post(pattern string, handler func(w http.ResponseWriter, r *http.Request)) {
+	http.HandleFunc(pattern, func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "POST" {
+			handler(w, r)
+		} else {
+			writeErrorWithCode(w, http.StatusMethodNotAllowed, "Method is not allowed")
+		}
+	})
+}
+func get(pattern string, handler func(w http.ResponseWriter, r *http.Request)) {
+	http.HandleFunc(pattern, func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "GET" {
+			handler(w, r)
+		} else {
+			writeErrorWithCode(w, http.StatusMethodNotAllowed, "Method is not allowed")
+		}
+	})
+}
